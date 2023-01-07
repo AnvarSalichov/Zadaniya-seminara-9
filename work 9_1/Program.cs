7093203ad6ed87b5e55f94699ef4add7636304f0
Anvar <anv@list.com>
2023-01-07 22:29:56 +0400

    задание1 (вывод чисел от заданного до 1 с помощью рекурсии)

--- work 9_1/Program.cs
+++ work 9_1/Program.cs
@@ -4,3 +4,24 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Ведите натуральное число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 1)
+{
+    Console.WriteLine("Введите значение больше 1");
+    return;
+}
+
+string PrintNum(int start, int end)
+{
+    if (start==end)
+    {
+        return end.ToString();   
+    }
+    return (start+" "+PrintNum(start-1,end));
+}
+
+Console.WriteLine(PrintNum(n,1));
